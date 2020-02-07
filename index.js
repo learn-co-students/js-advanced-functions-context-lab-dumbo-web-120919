1693c3f29854ce5bd569ce2e4cf423f5ed8c6b72
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-02-07 10:14:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,51 @@
-/* Your Code Here */
+function createEmployeeRecord(array) {
+    let employeeRecordObject = {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: []
+};
+return employeeRecordObject;
+};
+
+function createEmployeeRecords(arrayOfArrays) {
+    let employeeRecordsArray = [];
+    arrayOfArrays.forEach( array => employeeRecordsArray.push(createEmployeeRecord(array)) )
+    return employeeRecordsArray;
+};
+
+function createTimeInEvent(dateStamp) {
+// dateStamp in "YYYY-MM-DD HHMM" format
+let timeInObject = {
+    type: "TimeIn",
+    hour: parseInt(dateStamp.slice(-4)),
+    date: dateStamp.slice(0, 10)
+}
+this.timeInEvents.push(timeInObject);
+return this;
+};
+
+function createTimeOutEvent(dateStamp) {
+let timeOutObj = {
+    type: "TimeOut",
+    hour: parseInt(dateStamp.slice(-4)),
+    date: dateStamp.slice(0, 10)
+};
+this.timeOutEvents.push(timeOutObj);
+return this;
+};
+
+function hoursWorkedOnDate(soughtDate) {
+    // date is in form "YYYY-MM-DD"
+    let timeInObj = this.timeInEvents.find( timeInEvent => timeInEvent.date === soughtDate);
+    console.log(this);
+    let timeOutObj = this.timeOutEvents.find( timeOutEvent => timeOutEvent.date === soughtDate);
+    let hoursWorked = Math.abs((timeOutObj.hour - timeInObj.hour) / 100);
+    return hoursWorked;
+};
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
